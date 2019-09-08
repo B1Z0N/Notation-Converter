735065e339c399f42e4304c13a97abecb374f234
B1Z0N <kolausf@gmail.com>
2019-09-08 21:51:34 +0300

    Added some methods to internal node class

--- include/syntax_tree.h
+++ include/syntax_tree.h
@@ -1,12 +1,27 @@
 #ifndef SYNTAX_TREE_H
 #define SYNTAX_TREE_H
 
+#include <algorithm>
+#include <iterator>
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include "arithmetic_notation.h"
 
 namespace notation_conv {
 namespace detail {
+
+template <typename C>
+C split(const std::string& s) {
+  C contatiner;
+  std::istringstream iss(s);
+  std::copy(std::istream_iterator<std::string>{iss},
+            std::istream_iterator < std::string{},
+            std::back_inserter{contatiner});
+
+  return contatiner;
+}
 
 class SyntaxTree {
  public:
@@ -18,8 +33,30 @@ class SyntaxTree {
 
  private:
   struct Node {
-    void nodify(const std::string& expr, ArithmeticNotation notation);
-    void stringify(ArithmeticNotation notation);
+    void nodify(const std::string& expr, ArithmeticNotation notation) {
+      switch (notation) {
+        case ArithmeticNotation::PREFIX: {
+          nodify_from_prefix(expr);
+          break;
+        }
+        case ArithmeticNotation::INFIX: {
+          nodify_from_infix(expr);
+          break;
+        }
+        case ArithmeticNotation::POSTFIX: {
+          nodify_from_postfix(expr);
+          break;
+        }
+      }
+    }
+
+    std::string stringify(ArithmeticNotation notation) {
+      switch (notation) {
+        case ArithmeticNotation::PREFIX: return stringify_to_prefix();
+        case ArithmeticNotation::INFIX: return stringify_to_infix();
+        case ArithmeticNotation::POSTFIX: return stringify_to_postfix();
+      }
+    }
 
    private:
     Node* left_;
@@ -30,10 +67,11 @@ class SyntaxTree {
     void nodify_from_infix(const std::string& expr);
     void nodify_from_postfix(const std::string& expr);
 
-    std::string stringify_to_prefix(ArithmeticNotation notation);
-    std::string stringify_to_infix(ArithmeticNotation notation);
-    std::string stringify_to_postfix(ArithmeticNotation notation);
+    std::string stringify_to_prefix();
+    std::string stringify_to_infix();
+    std::string stringify_to_postfix();
   };
+  Node* node;
 };
 
 };  // namespace detail
--- src/syntax_tree.cpp
+++ src/syntax_tree.cpp
@@ -7,10 +7,30 @@ namespace notation_conv {
 namespace detail {
 
 SyntaxTree::SyntaxTree(const std::string& expr, ArithmeticNotation notation) {
-
+  nodify(expr, notation);
 }
 
 std::string SyntaxTree::to_notation(ArithmeticNotation notation) {
+  return node.stringify(notation);
+}
+
+void SyntaxTree::Node::nodify_from_prefix(const std::string& expr) {
+
+}
+void SyntaxTree::Node::nodify_from_infix(const std::string& expr) {
+
+}
+void SyntaxTree::Node::nodify_from_postfix(const std::string& expr) {
+
+}
+
+std::string SyntaxTree::Node::stringify_to_prefix() {
+
+}
+std::string SyntaxTree::Node::stringify_to_infix() {
+
+}
+std::string SyntaxTree::Node::stringify_to_postfix() {
 
 }
 
